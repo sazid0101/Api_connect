7947e7d3290aeec99c16c00a3a637ddfd889b07a
sazid0101 <shahriarislamsazid@gmail.com>
2023-11-16 04:13:54 +0600

    show api data & add input field

--- js/index.js
+++ js/index.js
@@ -2,6 +2,37 @@ const loadPhone= async()=>{
     const response= await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     const apiData= await response.json();
     const phones=apiData.data;
-    console.log(phones)
+    // console.log(phones);
+    displayPhones(phones);
+}
+const displayPhones=phones=>{
+    // console.log(phones)
+    // step1:get the container element where i want to add the new element
+const phoneContainer=document.getElementById('phone-container')
+
+    phones.forEach(phone=>{
+        console.log(phone)
+        //step2: create child element
+        const phoneCard=document.createElement('div');
+        phoneCard.classList=`card w-96 bg-gray-400 shadow-xl mt-6 ml-6`;
+        // step3: set innerHtml
+        phoneCard.innerHTML=`
+    <figure><img class="pt-6" src=" ${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        <p>${phone.slug}</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary bg-emerald-700">Buy Now</button>
+        </div>
+    </div>
+        `;
+        // step4: append child
+        phoneContainer.appendChild(phoneCard)
+    })
+
+}
+
+const handleSearch= () =>{
+    console.log('handle')
 }
 loadPhone();
